2ca4c2561acaf4ab26a44d4675e20c10d2a2ab99
Kenneth Bolivar Castro <keboca@gmail.com>
2017-10-25 22:47:13 -0600

    Pruebas Abonados

---
+++ Pruebas/PruebasAbonados.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logica;
+using Datos;
+
+namespace Pruebas
+{
+    class PruebasAbonados
+    {
+        private IServiciosAbonados abonados = new AccionesAbonados();
+        private int Id;
+
+        public void ejecutar()
+        {
+            this.agregar();
+            this.listar();
+            this.encontrarPorId();
+            this.actualizar();
+            this.borrar();
+        }
+
+        public void agregar()
+        {
+            string Nombre = "AbonadoX";
+            this.abonados.agregar(Nombre, "PrimerApellido", "SegundoApellido", "Cedula", "Telefono", "Celular", "Direccion", "Correo", "NombreAbonado", true);
+            this.Id = new ASADAEntidades().Abonados
+                .Where(a => a.Nombre == Nombre)
+                .FirstOrDefault()
+                .Id;
+            Console.WriteLine("Nuevo abonado creado con ID: " + Id);
+        }
+
+        public void listar()
+        {
+            List<Abonado> lista = this.abonados.listar();
+            foreach (Abonado abonado in lista)
+            {
+                Console.WriteLine(abonado.Nombre);
+            }
+        }
+
+        public void encontrarPorId()
+        {
+            Abonado abonado = this.abonados.encontrarPorId(this.Id);
+            Console.WriteLine("Abonado encontrado: " + abonado.Nombre);
+        }
+
+        public void actualizar()
+        {
+            this.abonados.actualizar(this.Id, "NuevoNombre", "NuevoPrimerApellido", "NuevoSegundoApellido", "NuevaCedula", "NuevoTelefono", "NuevoCelular", "NuevaDireccion", "NuevoCorreo", "NuevoNumeroAbonado", false);
+            Console.WriteLine("Abonado actualizado.");
+        }
+
+        public void borrar()
+        {
+            this.abonados.borrar(this.Id);
+            Console.WriteLine("Abonado eliminado.");
+        }
+    }
+}
---
+++ Pruebas/PruebasPrevistas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logica;
+using Datos;
+
+namespace Pruebas
+{
+    class PruebasPrevistas
+    {
+
+        private int Id;
+    }
+}
