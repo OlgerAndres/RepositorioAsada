ae878f5cd3b89b316c8847fd395e4dc3492cd258
David Montero <davimont2009@hotmail.com>
2017-12-12 22:32:10 -0600

    reporte abonados listo

---
+++ Asada/ReporteAbonados.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Logica;
+using Microsoft.Reporting.WinForms;
+
+
+namespace Asada
+{
+    /// <summary>
+    /// Lógica de interacción para ReporteAbonados.xaml
+    /// </summary>
+    public partial class ReporteAbonados : Window
+    {
+        private bool _isReportViewerLoaded;
+
+        public ReporteAbonados()
+        {
+            InitializeComponent();
+            this._reportViewer2.Load += _reportViewer2_Load;
+        }
+
+        private void _reportViewer2_Load(object sender, EventArgs e)
+        {
+
+            if (_isReportViewerLoaded)
+            {
+                return;
+            }
+
+            this._reportViewer2.ProcessingMode = Microsoft.Reporting.WinForms.ProcessingMode.Local;
+            AccionesAbonados abonado = new AccionesAbonados();
+
+
+            Microsoft.Reporting.WinForms.ReportDataSource dataSource = new ReportDataSource("DataSet1", abonado.listar());
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+
+            this._reportViewer2.LocalReport.DataSources.Clear();
+            this._reportViewer2.LocalReport.DataSources.Add(dataSource);
+
+            this._reportViewer2.LocalReport.DataSources.Add(reportDataSource1);
+            this._reportViewer2.LocalReport.ReportEmbeddedResource = "Asada.Report1.rdlc";
+
+            _reportViewer2.RefreshReport();
+            _isReportViewerLoaded = true;
+
+        }
+
+
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+          
+        }
+
+    }
+}
